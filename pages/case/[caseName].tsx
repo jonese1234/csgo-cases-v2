4be9a3ed7aa3bae09739e52418429c1f9185b5cb
Rhys Jones <rjrhys10@googlemail.com>
2022-03-12 18:14:04 +0000

    Hopefully fixed static page issue

--- pages/case/[caseName].tsx
+++ pages/case/[caseName].tsx
@@ -1,38 +1,29 @@
 import { useRouter } from "next/router";
-import { GetCaseData } from "../../components/shared/api-manager";
+import { GetAllCasesData, GetCaseData } from "../../components/shared/api-manager";
 import  Error from "next/error"
 import { CaseMapper } from "../../components/CasePage/models/case.model";
 import CaseHeader from "../../components/CasePage/CaseHeader/CaseHeader";
 import CaseWrapper from "../../components/CasePage/CaseContent/CaseWrapper";
 import styles from '../../styles/case.module.css'
 import Head from "next/head";
+import { GetStaticPaths, GetStaticProps } from 'next';
 
 
-function CasePage() {
-    const router = useRouter();
-    const { caseName } = router.query;
-    console.log("Page", caseName)
-    if(caseName === undefined) return <div>Loading...</div>
-
-    let cn = caseName as string;
-    var replaced = cn.split('-').join(' ');
-    const { data, isLoading, isError } = GetCaseData(replaced as string);
-
-    console.log("Page", data)
-    console.log("Page", isLoading)
-    console.log("Page", isError)
-
-    if (isLoading) return <div>Loading...</div>
-   //if(isError) return <Error statusCode={404} />
-
+function CasePage({ data }: {data: any}) {
+    //if (isLoading) return <div>Loading...</div>
+    //if(isError) return <Error statusCode={404} />
+    const router = useRouter()
+    if (!router.isFallback && !data) {
+      return <Error statusCode={404} />
+    }
+    
     let c = CaseMapper(data);
-    console.log(c);
 
     return(
         <div className={styles.background}>
                   <Head>
-                    <title>{replaced}</title>
-                    <meta name="description" content={replaced}/>
+                    <title>{c.name}</title>
+                    <meta name="description" content={c.name}/>
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
             <main className={styles.container}>
@@ -42,5 +33,27 @@ function CasePage() {
         </div>
     );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const res = await fetch('https://raw.githubusercontent.com/jonese1234/Csgo-Case-Data/master/latest.json');
+    const posts = await res.json();
+    const data = posts['cases'];
+    let paths = [];
+    for (const x in data) {
+        paths.push({params: { caseName: (data[x]['name']).split(' ').join('-') }});
+    }
+    return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+    const res = await fetch('https://raw.githubusercontent.com/jonese1234/Csgo-Case-Data/master/latest.json');
+    const posts = await res.json();
+    const cases = posts['cases'];
+
+    const cn = context.params?.caseName as string || '';
+    var replaced = cn.split('-').join(' ');
+    const data = cases[replaced]
+    return { props: { data: data ?? null } };
+};
 
 export default CasePage;
