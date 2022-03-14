054c2dd3e3d50e6793044bf47315678e63864988
Rhys Jones <rjrhys10@googlemail.com>
2022-03-14 23:05:47 +0000

    update to subdomain

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/csgo-cases-v2',
-  assetPrefix: '/csgo-cases-v2/',
+  //basePath: '/csgo-cases-v2',
+  //assetPrefix: '/csgo-cases-v2/',
 }
 
 module.exports = nextConfig
