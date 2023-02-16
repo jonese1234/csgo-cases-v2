f6b6cafb2576741ad63ae1322320ce1f0ba25203
Rhys Jones <rjrhys10@googlemail.com>
2023-02-16 23:16:28 +0000

    Add support button

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,6 +9,7 @@ function Header() {
                 <Link href="/" passHref><span style={{cursor: 'pointer'}}><HomeIcon fontSize="large" /></span></Link>
             </Box>
             <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center',  marginRight: '1rem', fontWeight: 700, fontFamily: 'Montserrat'}}>
+                <a href='https://github.com/sponsors/jonese1234' target="_blank" rel="noopener noreferrer" style={{marginRight: '1rem', textDecoration: 'none'}}>Support This Project</a>
                 <Link href="https://jonesy.moe" passHref><span style={{cursor: 'pointer', textDecoration: 'none'}}>JONESY.MOE</span></Link>
             </Box>
         </Box>
