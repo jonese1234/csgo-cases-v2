ae2af5e927ff2bd11d2d3c296b81035235fb0a8c
Rhys Jones <rjrhys10@googlemail.com>
2023-03-01 01:18:46 +0000

    Donation link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,7 +9,8 @@ function Header() {
                 <Link href="/" passHref><span style={{cursor: 'pointer'}}><HomeIcon fontSize="large" /></span></Link>
             </Box>
             <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: 'center',  marginRight: '1rem', fontWeight: 700, fontFamily: 'Montserrat'}}>
-                <a href='https://github.com/sponsors/jonese1234' target="_blank" rel="noopener noreferrer" style={{marginRight: '1rem', textDecoration: 'none'}}>Support This Project</a>
+                <a href='https://steamcommunity.com/tradeoffer/new/?partner=84809161&token=FddS8eDl' target="_blank" rel="noopener noreferrer" style={{marginRight: '1rem', textDecoration: 'none'}}>Donate skins here</a>
+                <a href='https://github.com/sponsors/jonese1234' target="_blank" rel="noopener noreferrer" style={{marginRight: '1rem', textDecoration: 'none'}}>Support this project</a>
                 <Link href="https://jonesy.moe" passHref><span style={{cursor: 'pointer', textDecoration: 'none'}}>JONESY.MOE</span></Link>
             </Box>
         </Box>
